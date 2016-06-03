4c8b0a3646c1ace20fc4c326a73a9062b7a52a02
afei1231 <afei1231@gmail.com>
2016-06-03 16:16:24 +0800

    puma config

---
+++ config/puma.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/env puma
+
+# rails environment
+environment 'production'
+port 3000
+threads 0, 16
+workers 1
+
+# application name, application path
+app_name = "afeilog"
+# application_path = "/var/www/afeilog"
+application_path = "/Users/ichr/Rails/afeilog"
+directory "#{application_path}"
+
+# puma configration
+pidfile "#{application_path}/tmp/puma.pid"
+state_path "#{application_path}/tmp/puma.state"
+stdout_redirect "#{application_path}/log/puma.stdout.log", "#{application_path}/log/puma.stderr.log"
+bind "unix://#{application_path}/tmp/#{app_name}.sock"
+activate_control_app "unix://#{application_path}/tmp/pumactl.sock"
+
+# run as daemonize
+daemonize true
+on_restart do
+  puts 'On restart...'
+end
+
+preload_app!
+
+rackup DefaultRackup
+
+on_worker_boot do
+  ActiveRecord::Base.establish_connection
+end
