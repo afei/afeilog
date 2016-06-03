de212b2a9e5b8cc6718d58f8d7fb29d8bf251ea1
afei1231 <afei1231@gmail.com>
2016-06-03 15:08:01 +0800

    init

---
+++ app/controllers/afeil_controller.rb
@@ -0,0 +1,4 @@
+class AfeilController < ApplicationController
+	def index
+	end
+end
---
+++ test/controllers/afeil_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class AfeilControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
